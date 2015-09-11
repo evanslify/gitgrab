a032a775c44dfd8e12aaec7f44e157cb6aac62c4
evanslify <alpha.bhk@gmail.com>
2015-09-11 16:52:29 +0800

    remove testwget

--- grab/spiders/grabber.py
+++ grab/spiders/grabber.py
@@ -1,13 +1,30 @@
-# -*- coding: utf-8 -*-
+# working :(
+
 import scrapy
 
 
-class GrabberSpider(scrapy.Spider):
-    name = "grabber"
-    allowed_domains = ["127.0.0.1"]
-    start_urls = (
-        'http://www.127.0.0.1/',
-    )
+class grabber(scrapy.Spider):
+    name = 'grabber'
+
+    def __init__(self, *args, **kwargs):
+        super(grabber, self).__init__(*args, **kwargs)
+        self.target_url = kwargs.get('url')
+        if self.target_url == None:
+            raise Exception('No URL entered.')
+
+    def start_requests(self):
+        yield scrapy.Request(url='file://\/dev/null', callback=self.parse)
+
+        #  yield scrapy.Request(url='', callback=self.parse)
+    #  def get(self, response)j:
+        #  return
+
+    # start_urls = []
+
+    # def __init__(self, *args, **kwargs):
+    #     super(testwget, self).__init__(*args, **kwargs)
+    #     self.start_urls = [kwargs.get('start_url')]
 
     def parse(self, response):
-        pass
+        items = {'file_urls': [self.target_url]}
+        return items
